6b78fee7b670d39a31b2e54074162d487a003583
Aakash Gajjar <102506089+akash-gajjar@users.noreply.github.com>
2023-04-22 21:57:06 +0530

    build(webpack): improve webpack and packages

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,57 +9,65 @@ const commonConfig = require('./webpack.config.common');
 require('dotenv').config({ path: './.env' });
 
 // check if running inside AWS CodeBuild
-const BUILD_ENV = process.env.CODEBUILD_BUILD_ID;
+const isCI = process.env.CODEBUILD_BUILD_ID;
 // include only variable used in our application
-const APP_ENV = pickBy(process.env, (_, key) => key.match(/^APP/));
+const env = pickBy(process.env, (_, key) => key.match(/^APP/));
 
-module.exports = merge(commonConfig, {
+/**
+ * @typedef {import('webpack').Configuration} Configuration
+ * @type {Configuration}
+ *
+ * @see https://webpack.js.org/configuration/
+ */
+const config = {
   mode: 'production',
-  stats: 'verbose',
+  stats: 'detailed',
   plugins: [
     new webpack.DefinePlugin({
       process: {
-        env: JSON.stringify(APP_ENV),
+        env: JSON.stringify(env),
       },
     }),
-    BUILD_ENV &&
-    new CompressionPlugin({
-      test: /\.(js|css)$/,
-      filename: '[path][base]',
-      algorithm: 'gzip',
-      deleteOriginalAssets: true,
-    }),
-    BUILD_ENV &&
-    new S3Plugin({
-      s3Options: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      },
-      s3UploadOptions: {
-        ACL: '',
-        Bucket: process.env.FRONTEND_BUCKET_NAME, // Your bucket name
-        // eslint-disable-next-line consistent-return
-        ContentEncoding(fileName) {
-          if (/\.(js|css)$/.test(fileName)) {
-            return 'gzip';
-          }
+    isCI &&
+      new CompressionPlugin({
+        test: /\.(js|css)$/,
+        filename: '[path][base]',
+        algorithm: 'gzip',
+        deleteOriginalAssets: true,
+      }),
+    isCI &&
+      new S3Plugin({
+        s3Options: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         },
-        // eslint-disable-next-line consistent-return
-        ContentType(fileName) {
-          if (/\.css/.test(fileName)) {
-            return 'text/css';
-          }
+        s3UploadOptions: {
+          ACL: '',
+          Bucket: process.env.FRONTEND_BUCKET_NAME, // Your bucket name
+          // eslint-disable-next-line consistent-return
+          ContentEncoding(fileName) {
+            if (/\.(js|css)$/.test(fileName)) {
+              return 'gzip';
+            }
+          },
+          // eslint-disable-next-line consistent-return
+          ContentType(fileName) {
+            if (/\.css/.test(fileName)) {
+              return 'text/css';
+            }
 
-          if (/\.js/.test(fileName)) {
-            return 'application/javascript';
-          }
+            if (/\.js/.test(fileName)) {
+              return 'application/javascript';
+            }
+          },
         },
-      },
-      cloudfrontInvalidateOptions: {
-        DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
-        Items: ['/*'],
-      },
-      directory: 'dist', // This is the directory you want to upload
-    }),
+        cloudfrontInvalidateOptions: {
+          DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
+          Items: ['/*'],
+        },
+        directory: 'dist', // This is the directory you want to upload
+      }),
   ].filter(Boolean),
-});
+};
+
+module.exports = merge(commonConfig, config);
